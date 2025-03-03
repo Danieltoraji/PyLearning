1190bf30e0cf91527196d100bdb7965e1ed4ead1
Danieltoraji <98793646+Danieltoraji@users.noreply.github.com>
2025-03-03 22:36:44 +0800

    Delete python directory
    upload failed=(

--- python/2.简易实际项目/2.时钟/2.自动时钟 - 副本 (2).py
+++
@@ -1,8 +0,0 @@
-import datetime
-import time
-a = 0
-time1 = datetime.datetime.now()
-while 4 > 3:
-    time2 = datetime.datetime.now()
-    a = a + 1
-    print(time2 - time1,a)
--- python/2.简易实际项目/useless/clr.py
+++
@@ -1,2 +0,0 @@
-f=open('res.txt','a')
-f.truncate(0)
